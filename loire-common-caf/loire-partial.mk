5d178baa41f941a631684faa4e94ba58d7e15fed
Erik Castricum <git@cas-online.nl>
2017-06-05 20:27:35 +0200

    loire: donot dexpreopt prebuilt jars and apks
    Change-Id: I9d8fe4c6e9289f0269c03fb2f346d66acfaea32d

--- loire-common-caf/loire-partial.mk
+++ loire-common-caf/loire-partial.mk
@@ -14,24 +14,39 @@
 
 # shared libraries
 PRODUCT_PACKAGES += \
-    libloc_api_v02 \
-    libloc_ds_api
-
-# framework
-PRODUCT_PACKAGES += \
-    qcnvitems \
-    qcrilhook
-
-# app
-PRODUCT_PACKAGES += \
-    fastdormancy \
-    ims \
-    imssettings \
-    QtiTelephonyService \
-    TimeService
-
-# priv-pp
-PRODUCT_PACKAGES += \
-    CNEService \
-    com.qualcomm.location \
-    qcrilmsgtunnel
+	libstagefright_soft_flacdec \
+	libpreload-me_so_32 \
+	libloc_api_v02 \
+	libloc_ds_api \
+	liblbs_core \
+	libpreload-me_so_64 \
+	QtiTelephonyService \
+	TimeService \
+	fastdormancy \
+	ad_calib \
+	SwimConfig \
+	andsfCne \
+	dpm \
+	NsrmConfiguration \
+	flashled_calc_parameters \
+	iddd \
+	cneapiclient_xml \
+	com_qti_location_sdk_xml \
+	com_qualcomm_location_xml \
+	com_quicinc_cne_xml \
+	imscm \
+	izat_xt_srv_xml \
+	qcnvitems_xml \
+	qcrilhook_xml \
+	qti_permissions \
+	cneapiclient_jar \
+	com_qti_location_sdk_jar \
+	com_quicinc_cne_jar \
+	imscmlibrary \
+	izat_xt_srv_jar \
+	qcnvitems_jar \
+	qcrilhook_jar \
+	rcsimssettings \
+	CNEService \
+	com_qualcomm_location_apk \
+	qcrilmsgtunnel
