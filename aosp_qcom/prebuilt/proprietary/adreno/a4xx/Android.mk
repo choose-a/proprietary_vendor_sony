d35f87c4b4c0a2e4421e26ceb415e2c33c1dbbe7
Erik Castricum <git@cas-online.nl>
2017-08-10 22:51:50 +0200

    [DONOT MERGE]create fake kanuti blob dir
    Change-Id: Ia70b1995a599cf50bc039b953ab1f6b8a43c3d05

--- aosp_qcom/prebuilt/proprietary/adreno/a4xx/Android.mk
+++ aosp_qcom/prebuilt/proprietary/adreno/a4xx/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(SONY_AOSP),true)
 
-ifneq ($(filter kanuti kitakami, $(PRODUCT_PLATFORM)), )
+ifneq ($(filter kitakami, $(PRODUCT_PLATFORM)), )
 include $(CLEAR_VARS)
 LOCAL_MODULE := a420_pfp
 LOCAL_MODULE_OWNER := Sony Mobile
@@ -521,7 +521,7 @@ LOCAL_MULTILIB := 64
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/lib64
 include $(BUILD_PREBUILT)
 endif
-ifneq ( ,$(filter $(PRODUCT_PLATFORM),kanuti kitakami))
+ifneq ( ,$(filter $(PRODUCT_PLATFORM),kitakami))
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 endif
